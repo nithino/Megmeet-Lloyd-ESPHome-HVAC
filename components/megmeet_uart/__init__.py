6b48b1058b9b07ab6d1303711ef312d9a45c7c66
nithino <nithino@gmail.com>
2026-06-29 02:08:30 +0530

    Update __init__.py

--- components/megmeet_uart/__init__.py
+++ components/megmeet_uart/__init__.py
@@ -1,5 +1,34 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+from esphome.const import CONF_ID
 
 CODEOWNERS = ["@nithino"]
 
+AUTO_LOAD = ["uart"]
+DEPENDENCIES = ["uart"]
+
 megmeet_uart_ns = cg.esphome_ns.namespace("megmeet_uart")
+
+MegmeetUART = megmeet_uart_ns.class_(
+    "MegmeetUART",
+    cg.Component,
+    uart.UARTDevice,
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(MegmeetUART),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
