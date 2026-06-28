74d00724d1f85321a83b2e05c23c4a54b1dd7d00
nithino <nithino@gmail.com>
2026-06-28 18:40:53 +0530

    Create sensor.py

---
+++ components/megmeet_uart/sensor.py
@@ -0,0 +1,33 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+from esphome.components import uart
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ["uart"]
+
+megmeet_ns = cg.esphome_ns.namespace("megmeet_uart")
+
+MegmeetUART = megmeet_ns.class_(
+    "MegmeetUART",
+    cg.Component,
+    uart.UARTDevice,
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(MegmeetUART),
+        }
+    )
+    .extend(uart.UART_DEVICE_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+
+    await cg.register_component(var, config)
+
+    await uart.register_uart_device(var, config)
